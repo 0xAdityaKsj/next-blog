27f4e436275339650750d53573facb08a9c47043
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2024-10-24 15:08:01 +0530

    changess more changesss

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,25 +1,34 @@
-const Page = () => {
-    return (
-        <main className="text-center pt-16 px-5">
-            <h1 className="text-4xl md:text-5xl font-bold mb-5 text-violet-400 transition duration-300 ease-in-out hover:text-violet-600 hover:scale-105">
-                Create Post
-            </h1>
-            <form className="flex justify-center items-center space-x-4">
-                <input
-                    type="text"
-                    placeholder="Enter title"
-                    className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
-                    required
-                />
-                <button
-                    type="submit"
-                    className="px-4 py-2 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition duration-300 ease-in-out"
-                >
-                    Submit
-                </button>
-            </form>
-        </main>
-    );
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { createPost } from "../actions/actions";
+
+const Page = async () => {
+
+  return (
+    <main className="text-center pt-16 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5 text-violet-400 transition duration-300 ease-in-out hover:text-violet-600 hover:scale-105">
+        Create Post
+      </h1>
+      <form
+        action={createPost}
+        className="flex justify-center items-center space-x-4">
+
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter title"
+          className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          required
+        />
+        <button
+          type="submit"
+          className="px-4 py-2 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition duration-300 ease-in-out"
+        >
+          Submit
+        </button>
+        <LogoutLink>Logout</LogoutLink>
+      </form>
+    </main>
+  );
 }
 
 export default Page;

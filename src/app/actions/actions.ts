904f7d5841636f1050e67781d0faa040e45cfadb
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2024-10-31 15:32:07 +0530

    feat: add text area, add schema, update actions

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,9 +1,11 @@
 "use server";
+
 export async function createPost(formData: FormData) {
 
   const title = formData.get("title") as string;
+  const body = formData.get("content") as string;
 
-  console.log(title);
+  console.log(title, body);
 
 
 
--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,8 +1,7 @@
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { createPost } from "../actions/actions";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 const Page = async () => {
-
   return (
     <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5 text-violet-400 transition duration-300 ease-in-out hover:text-violet-600 hover:scale-105">
@@ -10,25 +9,35 @@ const Page = async () => {
       </h1>
       <form
         action={createPost}
-        className="flex justify-center items-center space-x-4">
-
+        className="flex flex-col justify-center items-center space-y-4"
+      >
         <input
           type="text"
           name="title"
           placeholder="Enter title"
-          className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          className="w-full max-w-md px-4 py-3 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-violet-500 transition duration-300 ease-in-out hover:shadow-lg"
+          required
+        />
+        <textarea
+          name="content"
+          placeholder="Enter body"
+          className="w-full max-w-md border border-gray-300 rounded-md shadow-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-violet-500 transition duration-300 ease-in-out hover:shadow-lg"
+          rows="5"
           required
         />
         <button
           type="submit"
-          className="px-4 py-2 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition duration-300 ease-in-out"
+          className="px-6 py-3 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition duration-300 ease-in-out shadow-md hover:shadow-lg"
         >
           Submit
         </button>
-        <LogoutLink>Logout</LogoutLink>
+        <LogoutLink className="text-violet-500 hover:text-violet-600 transition duration-300 ease-in-out">
+          Logout
+        </LogoutLink>
       </form>
     </main>
   );
-}
+};
 
 export default Page;
+

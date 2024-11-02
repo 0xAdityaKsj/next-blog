435e910e587fa1f8b8a868b1cf331ac8bdef88e2
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2024-11-03 01:28:34 +0530

    hmmm

--- src/app/posts/[id]/post-item.tsx
+++ src/app/posts/[id]/post-item.tsx
@@ -1,17 +1,23 @@
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import prisma from "@/lib/db";
 
 const PostItem = async ({ params }: { params: Params }) => {
-    const response = await fetch(`https://dummyjson.com/posts/${params.id}`);
-    const post = await response.json();
-    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-    await delay(2000); // 2 second delay
+  const post = await prisma.post.findUnique({
+    where: {
+      id: parseInt(params.id)
+    }
+  });
 
-    return (
-        <div className="text-center px-5 pt-24">
-            <h1 className="pb-5 text-5xl font-bold">{post.title}</h1>
-            <p className="mx-auto max-w-[700px]">{post.body}</p>
-        </div>
-    );
-}
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <div className="text-center px-5 pt-24">
+      <h1 className="pb-5 text-5xl font-bold">{post.title}</h1>
+      <p className="mx-auto max-w-[700px]">{post.body}</p>
+    </div>
+  );
+};
 
 export default PostItem;
--- src/app/posts/post-list.tsx
+++ src/app/posts/post-list.tsx
@@ -1,24 +1,21 @@
 import Link from "next/link";
+import prisma from "@/lib/db";
 
 const PostList = async () => {
-    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-    // Simulate a delay to demonstrate loading
-    await delay(2000);
+  const posts = await prisma.post.findMany();
 
-    const response = await fetch("https://dummyjson.com/posts?limit=10");
-    const data = await response.json();
-    return (
-        <ul className="space-y-2">
-            {data.posts.map(post => (
-                <li key={post.id} className="transform transition-all duration-300 ease-in-out hover:translate-x-2">
-                    <Link href={`/posts/${post.id}`} className="block py-2 px-3 text-zinc-800 hover:text-violet-600 transition-colors duration-300">
-                        {post.title}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="space-y-2">
+      {posts.map(post => (
+        <li key={post.id} className="transform transition-all duration-300 ease-in-out hover:translate-x-2">
+          <Link href={`/posts/${post.id}`} className="block py-2 px-3 text-zinc-800 hover:text-violet-600 transition-colors duration-300">
+            {post.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default PostList;
